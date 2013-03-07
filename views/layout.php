5f1cf65bb9a838866eaaed670d80abff1223c0fb
filp <dobreira@gmail.com>
2013-03-07 11:25:35 +0000

    Sanitize data from JSON header

--- views/layout.php
+++ views/layout.php
@@ -1,12 +1,19 @@
+<?php
+// Sanitize html content:
+function e($dirty) {
+    return htmlspecialchars($dirty, ENT_QUOTES, 'UTF-8');
+}
+
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
 
         <?php if($page['title'] === false): ?>
-            <title><?php echo APP_NAME ?></title>
+            <title><?php echo e(APP_NAME) ?></title>
         <?php else: ?>
-            <title><?php echo $page['title'] ?> - <?php echo APP_NAME ?></title>
+            <title><?php echo e($page['title']) ?> - <?php echo e(APP_NAME) ?></title>
         <?php endif ?>
 
         <base href="<?php echo BASE_URL; ?>/">
@@ -17,11 +24,11 @@
         <link rel="stylesheet" href="static/css/codemirror.css">
         <link rel="stylesheet" href="static/css/main.css">
 
-        <meta name="description" content="<?php echo $page['description'] ?>">
-        <meta name="keywords" content="<?php echo join(',', $page['tags']) ?>">
+        <meta name="description" content="<?php echo e($page['description']) ?>">
+        <meta name="keywords" content="<?php echo e(join(',', $page['tags'])) ?>">
 
         <?php if(!empty($page['author'])): ?>
-            <meta name="author" content="<?php echo $page['author'] ?>"
+            <meta name="author" content="<?php echo e($page['author']) ?>"
         <?php endif; ?>
 
         <script src="static/js/jquery.min.js"></script>
@@ -40,7 +47,7 @@
                     <div class="span3">
                         <div id="sidebar">
                             <div class="inner">
-                                <h2><span><?php echo APP_NAME ?></span></h2>
+                                <h2><span><?php echo e(APP_NAME) ?></span></h2>
                                 <?php include('tree.php') ?>
                             </div>
                         </div>
