2ed56a598d0a223b5c66a5c2f7d931383f2e1721
Victor Stanciu <vic.stanciu@gmail.com>
2013-03-13 11:20:26 +0200

    fix short open tag

--- views/render.php
+++ views/render.php
@@ -19,7 +19,6 @@
                 <a href="<?php echo htmlspecialchars($url, ENT_QUOTES, 'UTF-8') ?>">
                     <?php if (++$i == count($parts) && !$is_dir): ?>
                         <i class="icon-file icon-white"></i>
-
                     <?php else: ?>
                         <i class="icon-folder-open icon-white"></i>
                     <?php endif ?>
@@ -46,81 +45,82 @@
     </script>
 <?php endif ?>
 
-<?php if(isset($source)): ?>
-<div id="source">
-    <?php if(ENABLE_EDITING): ?>
-        <div class="alert alert-info">
-            <i class="icon-pencil"></i> <strong>Editing is enabled</strong>. Use the "Save changes" button below the editor to commit modifications to this file.
-        </div>
-    <?php endif ?>
-    <form method="POST" action="<?php echo BASE_URL . "/?a=edit" ?>">
-        <input type="hidden" name="ref" value="<?php echo base64_encode($page['file']) ?>">
-        <textarea id="editor" name="source" class="input-block-level" rows="<?php echo substr_count($source, "\n") + 1; ?>"><?php echo $source; ?></textarea>
-
-        <?php if(ENABLE_EDITING): ?>
-        <div class="edit-dashboard">
-            <input type="submit" class="btn btn-inverse" value="Save Changes">
-        </div>
+<?php if (isset($source)): ?>
+    <div id="source">
+        <?php if (ENABLE_EDITING): ?>
+            <div class="alert alert-info">
+                <i class="icon-pencil"></i> <strong>Editing is enabled</strong>. Use the "Save changes" button below the editor to commit modifications to this file.
+            </div>
         <?php endif ?>
-    </form>
-</div>
 
-<script>
-    <?php if ($html) { ?>
-        CodeMirror.defineInitHook(function () {
-            $('#source').hide();
-        });
-    <?php } ?>
+        <form method="POST" action="<?php echo BASE_URL . "/?a=edit" ?>">
+            <input type="hidden" name="ref" value="<?php echo base64_encode($page['file']) ?>">
+            <textarea id="editor" name="source" class="input-block-level" rows="<?php echo substr_count($source, "\n") + 1; ?>"><?php echo $source; ?></textarea>
 
-    var mode = false;
-    var modes = {
-        'md': 'markdown',
-        'js': 'javascript',
-        'php': 'php',
-        'sql': 'text/x-sql',
-        'py': 'python',
-        'clj': 'clojure',
-        'rb': 'ruby',
-        'css': 'css',
-        'hs': 'haskell',
-        'lsh': 'haskell',
-        'pl': 'perl',
-        'r': 'r',
-        'scss': 'sass',
-        'sh': 'shell',
-        'xml': 'xml',
-        'html': 'htmlmixed',
-        'htm': 'htmlmixed'
-    };
-    var extension = '<?php echo $extension ?>';
-    if (typeof modes[extension] != 'undefined') {
-        mode = modes[extension];
-    }
+            <?php if (ENABLE_EDITING): ?>
+                <div class="edit-dashboard">
+                    <input type="submit" class="btn btn-inverse" value="Save Changes">
+                </div>
+            <?php endif ?>
+        </form>
+    </div>
 
-    var editor = $('#editor');
-    editor.prop('data-editor', CodeMirror.fromTextArea(editor[0], {
-        mode: mode,
-        theme: 'default',
-        lineNumbers: true,
-        lineWrapping: true,
-        <?php if(!ENABLE_EDITING): ?>
-        readOnly: true
-        <? endif ?>
-    }));
+    <script>
+        <?php if ($html): ?>
+            CodeMirror.defineInitHook(function () {
+                $('#source').hide();
+            });
+        <?php endif ?>
 
-    $('#toggle').on('click', function (event) {
-        event.preventDefault();
-
-        var source = $('#source');
-        var render = $('#render');
-        var submit = $('#submit-edit');
-
-        source.toggle();
-        render.toggle();
-
-        if (source.is(':visible')) {
-            $('#editor').prop('data-editor').refresh();
+        var mode = false;
+        var modes = {
+            'md': 'markdown',
+            'js': 'javascript',
+            'php': 'php',
+            'sql': 'text/x-sql',
+            'py': 'python',
+            'clj': 'clojure',
+            'rb': 'ruby',
+            'css': 'css',
+            'hs': 'haskell',
+            'lsh': 'haskell',
+            'pl': 'perl',
+            'r': 'r',
+            'scss': 'sass',
+            'sh': 'shell',
+            'xml': 'xml',
+            'html': 'htmlmixed',
+            'htm': 'htmlmixed'
+        };
+        var extension = '<?php echo $extension ?>';
+        if (typeof modes[extension] != 'undefined') {
+            mode = modes[extension];
         }
-    });
-</script>
+
+        var editor = $('#editor');
+        editor.prop('data-editor', CodeMirror.fromTextArea(editor[0], {
+            mode: mode,
+            theme: 'default',
+            lineNumbers: true,
+            lineWrapping: true
+            <?php if(!ENABLE_EDITING): ?>
+                ,readOnly: true
+            <?php endif ?>
+        }));
+
+        $('#toggle').on('click', function (event) {
+            event.preventDefault();
+
+            var source = $('#source');
+            var render = $('#render');
+            var submit = $('#submit-edit');
+
+            source.toggle();
+            render.toggle();
+
+            if (source.is(':visible')) {
+                $('#editor').prop('data-editor').refresh();
+            }
+        });
+    </script>
 <?php endif ?>
