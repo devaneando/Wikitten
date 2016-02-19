72e1c75cf804dc02daecea68336b45b47e045779
Praveen Kumar <praveenscience@gmail.com>
2016-02-19 23:08:32 +0000

    Added header for Uh Oh page.

--- views/uhoh.php
+++ views/uhoh.php
@@ -1,3 +1,15 @@
+<div class="breadcrumbs">    
+
+  <ul class="breadcrumb">
+    <li>
+      <a href="<?php echo BASE_URL; ?>"><i class="glyphicon glyphicon-home glyphicon-white"></i> /wiki</a>
+    </li>
+
+    <li>
+      <a href="<?php echo BASE_URL; ?>/404"><i class="glyphicon glyphicon-exclamation-sign glyphicon-white"></i> Error</a>
+    </li>
+  </ul>
+</div>
 <h1>Uh oh!</h1>
 <p>
   An <strong>error</strong> ocurred: <code><?php echo $error ?></code>
