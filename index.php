c945e38cb2eabe40d151d7529b4d20c3e06717c1
Victor Stanciu <vic.stanciu@gmail.com>
2016-02-23 12:00:26 +0200

    Fix PhpStorm inspections in main Wiki class

--- index.php
+++ index.php
@@ -2,36 +2,44 @@
 
 // Conditionally load configuration from a config.php file in
 // the site root, if it exists.
-if(is_file($config_file = __DIR__ . DIRECTORY_SEPARATOR . 'config.php')) {
+if (is_file($config_file = __DIR__ . DIRECTORY_SEPARATOR . 'config.php')) {
     require_once $config_file;
 }
 
-if(!defined('APP_NAME')) {
+if (!defined('APP_NAME')) {
     define('APP_NAME', 'Wikitten');
 }
 
-if(!defined('LIBRARY')) {
+if (!defined('LIBRARY')) {
     define('LIBRARY', __DIR__ . DIRECTORY_SEPARATOR . 'library');
 }
 
-if(!defined('DEFAULT_FILE')) {
+if (!defined('DEFAULT_FILE')) {
     define('DEFAULT_FILE', 'index.md');
 }
 
-if(!defined('USE_WIKITTEN_LOGO')) {
+if (!defined('USE_WIKITTEN_LOGO')) {
     define('USE_WIKITTEN_LOGO', true);
 }
 
-if(!defined('USE_DARK_THEME')) {
+if (!defined('USE_DARK_THEME')) {
     define('USE_DARK_THEME', false);
 }
 
-if(!defined('USE_PAGE_METADATA')) {
+if (!defined('USE_PAGE_METADATA')) {
     define('USE_PAGE_METADATA', true);
 }
 
-if(!defined('ENABLE_EDITING')) {
+if (!defined('ENABLE_EDITING')) {
     define('ENABLE_EDITING', false);
+}
+
+if (!defined('ENABLE_PASTEBIN')) {
+    define('ENABLE_PASTEBIN', false);
+}
+
+if (!defined('PASTEBIN_API_KEY')) {
+    define('PASTEBIN_API_KEY', false);
 }
 
 define('PLUGINS', __DIR__ . DIRECTORY_SEPARATOR . 'plugins');
