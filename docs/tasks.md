dbff66973ebb77657b2aaef04bf50b6d08a6068b
ray <rayjlim1@gmail.com>
2020-09-13 10:37:19 -0700

    ✨ feat: output json instead of html

---
+++ docs/tasks.md
@@ -0,0 +1,13 @@
+
+# Tasks
+
+- task: use constants for local vs. production
+- task: commit to bitbucket
+- idea: impl security token from ui to api; backend check for token; store token in localstorage
+- feat: tree view
+- idea: custom toolbar for mobile view
+- bug: readme.md from wikitten not rendering in editor
+- idea: can use checklist; mark completed with keyboard/button press
+- idea: validate/fix a document's errors; see readme.md for errors
+- task: redirect on file save; to reload view
+- idea: check if not saved before navigate away
--- views/layout.php
+++ views/layout.php
@@ -3,98 +3,22 @@ if (!defined('APP_STARTED')) {
     die('Forbidden!');
 }
 
+
+
 // Sanitize html content:
 function e($dirty)
 {
     return htmlspecialchars($dirty, ENT_QUOTES, 'UTF-8');
 }
+
+// var_dump($html);
+$output = new stdClass();
+$output->page = $page;
+$output->source = $source;
+
+
+echo json_encode($output);
+
+// include('tree.php')
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-
-        <?php if ($page['title'] === false) : ?>
-            <title><?php echo e(APP_NAME) ?></title>
-        <?php else: ?>
-            <title><?php echo e($page['title']) ?> - <?php echo e(APP_NAME) ?></title>
-        <?php endif ?>
-
-        <base href="<?php echo BASE_URL; ?>/">
-
-        <link rel="shortcut icon" href="static/img/favicon.ico">
-
-        <?php if (USE_DARK_THEME) : ?>
-            <link rel="stylesheet" href="static/css/bootstrap_dark.min.css">
-            <link rel="stylesheet" href="static/css/dark/prettify-dark.css">
-            <link rel="stylesheet" href="static/css/main_dark.css">
-            <link rel="stylesheet" href="static/css/dark/codemirror-tomorrow-night-bright.css">
-        <?php else: ?>
-            <link rel="stylesheet" href="static/css/bootstrap.min.css">
-            <link rel="stylesheet" href="static/css/prettify.css">
-            <?php if(!empty(CUSTOM_MARKDOWN_STYLESHEET) && (file_exists(__DIR__ . '/../static/css/custom-styles/'.CUSTOM_MARKDOWN_STYLESHEET))) : ?>
-                <link rel="stylesheet" href="static/css/custom-styles/<?php echo(CUSTOM_MARKDOWN_STYLESHEET) ?>">
-            <?php endif; ?>
-            <link rel="stylesheet" href="static/css/main.css">
-        <?php endif; ?>
-
-        <link rel="stylesheet" href="static/css/codemirror.css">
-        <link rel="stylesheet" href="static/css/all.min.css">
-        <link rel="stylesheet" href="static/css/custom.css">
-
-        <meta name="description" content="<?php echo e($page['description']) ?>">
-        <meta name="keywords" content="<?php echo e(join(',', $page['tags'])) ?>">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <?php if (!empty($page['author'])) : ?>
-            <meta name="author" content="<?php echo e($page['author']) ?>">
-        <?php endif; ?>
-
-        <script src="static/js/jquery.min.js"></script>
-        <script src="static/js/prettify.js"></script>
-        <script src="static/js/codemirror.min.js"></script>
-    </head>
-<body>
-    <div id="main">
-        <?php if (USE_WIKITTEN_LOGO === true): ?>
-            <a href="http://wikitten.vizuina.com" id="logo" target="_blank" class="hidden-phone">
-                <img src="static/img/logo.png" alt="">
-                <div class="bubble">Remember to check for updates!</div>
-            </a>
-        <?php endif; ?>
-        <div class="inner">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-xs-12 col-md-3">
-                        <div id="sidebar">
-                            <div class="inner">
-                                <h2><span><?php echo e(APP_NAME) ?></span></h2>
-                                <?php include('tree.php') ?>
-                            </div>
-                        </div>
-
-                        <a href="<?php echo BASE_URL; ?>?action=logout" class="btn btn-default btn-xs">Logout</a>
-                    </div>
-                    <div class="col-xs-12 col-md-9">
-                        <div id="content">
-                            <div class="inner">
-                                <?php echo $content; ?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <script>
-        <?php if (USE_WIKITTEN_LOGO === true): ?>
-            $(document).ready(function () {
-                $('#logo').delay(2000).animate({
-                    left: '20px'
-                }, 600);
-            });
-        <?php endif; ?>
-    </script>
-</body>
-</html>
+ 
